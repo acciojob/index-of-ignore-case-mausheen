dcaa349c452912346e3e7ea98c865e32b602aee0
MOHD MAUSHEEN <97398765+mausheen@users.noreply.github.com>
2024-10-14 21:14:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	let ans=s1.indexOf(s2);
+	return ans;
 }
 
-// Please do not change the code below
+//Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
